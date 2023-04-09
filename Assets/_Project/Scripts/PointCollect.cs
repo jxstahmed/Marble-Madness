4b0e91b7b8a969b15dbd2556c5f322090b3c4814
majd bousaad <bousaadmajd@gmail.com>
2023-04-09 22:36:36 +0200

    Showing Score

--- Assets/_Project/Scripts/PointCollect.cs
+++ Assets/_Project/Scripts/PointCollect.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PointCollect : MonoBehaviour
 {
-    [SerializeField] public int points;
+    [SerializeField] public int points = 0;
+    public Text score;
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +17,7 @@ public class PointCollect : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        score.text = points.ToString();
     }
 
 }
